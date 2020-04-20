05e4911a58c90a22579275a70eeb19b74beaa0bc
JohnsonNYC <jkow95@gmail.com>
2020-04-20 16:47:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+    return drivers.forEach(element => {
+    console.log(element.name)        
+    });
+}
+
+const logDriversByHometown = function(drivers,hometown){
+    drivers.forEach( driver =>{
+        if(driver.hometown === hometown){
+            console.log (driver.name)
+        };
+    });
+};
+
+const driversByRevenue = function (drivers) {
+    return drivers.slice().sort( (x,y) => {
+      return x.revenue - y.revenue;
+    });
+};
+
+const driversByName = function(drivers){
+    return drivers.slice().sort( (x,y) => { //
+        return x.name.localeCompare(y.name)
+    })
+}
+
+const totalRevenue = function (drivers) {
+    return drivers.reduce(function (total, currentDriver) {
+      return currentDriver.revenue + total;
+    }, 0);
+  };
+
+const averageRevenue = function(driver){
+    return totalRevenue(driver)/driver.length
+}
+
