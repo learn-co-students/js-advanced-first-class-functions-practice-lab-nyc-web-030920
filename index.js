0860fd960d293234e08d2cfab8b7c759d3cf9d1f
Yueying <wangyy395@gmail.com>
2020-04-20 18:23:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const logDriverNames = function(obj) {
+    obj.forEach(function(element) {
+        console.log(element.name)
+    })
+}
+
+const logDriversByHometown = function(obj, hometown) {
+    obj.forEach(function(element) {
+        if (element.hometown === hometown) {
+            console.log(element.name);
+        }
+    })
+}
+
+const driversByRevenue = function(obj) {
+    return obj.slice().sort(function(e1, e2) {
+        return e1.revenue - e2.revenue;
+    })
+}
+
+const driversByName = function(obj) {
+    return obj.slice().sort(function(e1, e2){
+        return e1.name.localeCompare(e2.name);
+    })
+}
+
+const totalRevenue = function(obj) {
+    return obj.reduce(function(total, element) {
+        return total + element.revenue;
+    }, 0)
+}
+
+const averageRevenue = function(obj) {
+    return totalRevenue(obj) / obj.length;
+}
